8c045a0e99c894d5a60b1ce72f6b4b5158385365
SAMIUR <samiurkhan94@gmail.com>
2025-03-22 23:47:23 +0600

    feat(auth):removed redundant app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AppConfigModule } from './modules/common/app.config.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { CustomersModule } from './modules/customer/customer.module';
@@ -8,7 +6,7 @@ import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [AppConfigModule, DatabaseModule, CustomersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
